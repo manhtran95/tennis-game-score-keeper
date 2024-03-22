a06023a83379fea6861187175ca9d1e36fec60fd
Manh <manhnus@gmail.com>
2024-03-22 14:59:26 +0700

    refactoring main func

--- tennis.go
+++ tennis.go
@@ -6,18 +6,17 @@ import "fmt"
 	This program simulates a score keeping system for a tennis game.
 */
 
-const 
-(
+const (
 	// list of points
-	ZERO = "0"
+	ZERO      = "0"
 	FIFTHTEEN = "15"
-	THIRTY = "30"
-	FORTY = "40"
-	ADVANCED = "A"
+	THIRTY    = "30"
+	FORTY     = "40"
+	ADVANCED  = "A"
 	// list of states
 	STATE_NORMAL = "StateNormal"
-	STATE_DEUCE = "StateDeuce"
-	STATE_END = "StateEnd"
+	STATE_DEUCE  = "StateDeuce"
+	STATE_END    = "StateEnd"
 )
 
 var display_mappings = map[int]string{
@@ -34,57 +33,61 @@ type Player struct {
 	score int
 }
 
-
 func main() {
 	// loop for games
-	for{
-		// initialize for the game
-		var input string
+	for {
+		/* initialize for the game
+			gameState: state of the game, either STATE_NORMAL, STATE_DEUCE or STATE_END
+			playerWinning, playerLosing: pointer to player winning or losing a particular point
+			winner: pointer to player winning the game
+			player1: first player of the game
+			player2: second player of the game
+		*/
 		gameState := STATE_NORMAL
 		var playerWinning, playerLosing, winner *Player
 		player1 := Player{
-			name: "Player1",
+			name:  "Player1",
 			score: 0,
 		}
 		player2 := Player{
-			name: "Player2",
+			name:  "Player2",
 			score: 0,
 		}
 
 		fmt.Println("Tennis game started.")
 		inputPlayerNames(&player1, &player2)
-		
+
 		// loop for points during the game
-		for ;gameState != STATE_END ;{
+		for gameState != STATE_END {
 			displayScore(player1, player2)
 			playerWinning, playerLosing = processPointWinning(&player1, &player2)
-			
+
 			switch gameState {
 			case STATE_NORMAL:
 				gameState, winner = processStateNormal(playerWinning, playerLosing)
 			case STATE_DEUCE:
-				switch playerWinning.score{
-				case 3:
-					if playerLosing.score == 3 {
-						playerWinning.score = 4
-					} else {
-						playerLosing.score = 3
-					}
-				case 4:
-					gameState = STATE_END
-					winner = playerWinning
-				}
-			}	
+				gameState, winner = processStateDeuce(playerWinning, playerLosing)
+			}
 		}
-		// process end of game
-		fmt.Printf("Congratz, *%s* has won the game!\n", winner.name)
-		fmt.Println(`New game?(input 'y' or 'n')`)
-		fmt.Scanf("%s", &input)
-		if input != "y" {
-			fmt.Println("Good bye!")
+
+		playing := processGameEnding(winner)
+		if !playing {
 			break
 		}
+	}
+	fmt.Println("Good bye!")
+}
+
+func processGameEnding(winner *Player) bool {
+	var input string
+	fmt.Printf("Congratz, *%s* has won the game!\n", winner.name)
+	fmt.Println(`New game?(input 'y' or 'n')`)
+	fmt.Scanf("%s", &input)
+	if input != "y" {
+		return false
+	} else {
 		fmt.Println("-----")
+		return true
 	}
 }
 
@@ -128,5 +131,21 @@ func processStateNormal(playerWinning *Player, playerLosing *Player) (string, *P
 		// other case
 		playerWinning.score++
 		return STATE_NORMAL, nil
+	}
+}
+
+func processStateDeuce(playerWinning *Player, playerLosing *Player) (string, *Player) {
+	switch playerWinning.score {
+	case 3:
+		if playerLosing.score == 3 {
+			playerWinning.score = 4
+		} else {
+			playerLosing.score = 3
+		}
+		return STATE_DEUCE, nil
+	case 4:
+		return STATE_END, playerWinning
+	default:
+		return "", nil
 	}
 }
